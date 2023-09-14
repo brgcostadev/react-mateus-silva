538a6f55f3dab9e65eb868660646b49f15dd62f7
Bruno Costa <br9uno@gmail.com>
2023-09-14 20:26:27 -0300

    React JSX

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+const category = 'Posts da semana';
+
 function App() {
   return (
     <>
       <h1>Componente App</h1>
-      <h2>Componente App</h2>
+      <h2>{category}</h2>
     </>
   );
 }
